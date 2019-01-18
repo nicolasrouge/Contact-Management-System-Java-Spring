5f877d7a0099ed1a798185862d3e8a4579678af4
Nico <nicolas.rouge777@gmail.com>
2019-01-18 11:18:38 +0100

    bug

--- src/service/IContactService.java
+++ src/service/IContactService.java
@@ -3,7 +3,6 @@ package service;
 import java.util.List;
 
 import org.lip6.struts.domain.Contact;
-<<<<<<< HEAD
 
 public interface IContactService {
 
@@ -18,19 +17,4 @@ public interface IContactService {
 	public int deleteContact(int id) throws Exception;
 	
 	public List<Contact> searchContact(String keyword);
-=======
-import org.lip6.struts.domain.PhoneNumber;
-
-public interface IContactService {
-	public Contact getContact(long id);
-	public List<PhoneNumber> getPhones(long id);
-	public boolean addGroup(String nomGroup);
-	public List<Contact> getListContacts();
-    public Contact displayContact(int idcontact);
-    public int deleteContact(long id);
-    public List<Contact> searchContact(String word);
-	String updateContact(Contact contact);
-	public void addContact(String nom, String prenom, String mail, String phonenumber, String street, String city,
-			String zip, String country);
->>>>>>> master
 }
